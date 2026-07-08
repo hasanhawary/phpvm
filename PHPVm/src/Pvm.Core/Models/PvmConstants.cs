e9969a16514a6ba6883515259cdb77422ba8d1a5
Elhawary <h.mohammad@wakeb.com>
2026-07-08 17:37:12 +0300

    updates

--- PHPVm/src/Pvm.Core/Models/PvmConstants.cs
+++
@@ -1,8 +0,0 @@
-namespace Pvm.Core.Models;
-
-public static class PvmConstants
-{
-    public const string Version = "1.0.0";
-    public const string GitHubRepositoryUrl = "https://github.com/hasanhawary/phpvm";
-    public const string GitHubReleasesApiUrl = "https://api.github.com/repos/hasanhawary/phpvm/releases/latest";
-}
--- PHPVm/src/Pvm.Core/Models/UpdateCheckInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace Pvm.Core.Models;
-
-public sealed record UpdateCheckInfo(
-    string CurrentVersion,
-    string LatestVersion,
-    bool IsUpdateAvailable,
-    string? DownloadUrl,
-    string? ReleaseNotes
-);
